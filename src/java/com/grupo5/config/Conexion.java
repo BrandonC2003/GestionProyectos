2a41a73201f1cee13bf74d71b4e0617e932250fd
Cristian8r <2526052021@mail.utec.edu.sv>
2023-11-14 09:41:15 -0600

    agregando

--- src/java/com/grupo5/config/Conexion.java
+++ src/java/com/grupo5/config/Conexion.java
@@ -10,7 +10,7 @@ public class Conexion {
     // Variables a utilizar
     private static final String USUARIO = "root";
     private static final String PASS = "";
-    private static final String URL = "jdbc:mysql://localhost:3307/gestionproyectos";
+    private static final String URL = "jdbc:mysql://localhost:3306/gestionproyectos";
     // variable para gestionar la conexion
     private static Connection conexion = null;
     
